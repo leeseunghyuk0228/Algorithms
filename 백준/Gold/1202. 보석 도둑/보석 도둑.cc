ad71cd1b53925357a9fe6c6156a3295f89e22bc5
leeseunghyuk0228 <67091436+leeseunghyuk0228@users.noreply.github.com>
2023-05-09 00:06:44 +0900

    [Gold II] Title: 보석 도둑, Time: 464 ms, Memory: 9352 KB -BaekjoonHub

---
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+struct j {
+    int weight, price;
+    bool operator < (struct j next) const {
+        if (weight < next.weight) return true;
+        if (weight > next.weight) return false;
+        return false;
+    }
+};
+vector <j> jews;
+
+int bags[300000];
+int N, M, idx = 0;
+long long res = 0;
+
+void input() {
+    cin >> N >> M;
+    for (int i = 0; i < N; i++) {
+        int w, p;
+        cin >> w >> p;
+        jews.push_back({ w,p });
+    }
+
+    for (int i = 0; i < M; i++)cin >> bags[i];
+
+    sort(bags, bags+M);
+    sort(jews.begin(), jews.end());
+
+}
+
+int main() {
+
+    input();
+
+    priority_queue<int> pq;
+
+    for (int i = 0; i < M; i++) {
+        for (int j = idx; j < N; j++) {
+            if (jews[j].weight > bags[i])break;
+            idx++; pq.push(jews[j].price);
+        }
+        if (!pq.empty())
+        {
+            res += pq.top(); pq.pop();
+        }
+    }
+
+    cout << res;
+    return 0;
+
+}
