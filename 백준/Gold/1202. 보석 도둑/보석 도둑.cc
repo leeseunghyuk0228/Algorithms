e9e83ce649e3ef29713cded8f4f5c9f97579960c
leeseunghyuk0228 <67091436+leeseunghyuk0228@users.noreply.github.com>
2023-05-09 00:17:08 +0900

    [Gold II] Title: 보석 도둑, Time: 212 ms, Memory: 6296 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.cc
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.cc
@@ -1,23 +1,21 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
+#include <stdio.h>
 #include <vector>
 #include <queue>
 #include <algorithm>
 using namespace std;
 
 pair<int, int> jews[300000];
-
 int bags[300000];
 int N, M, idx = 0;
 long long res = 0;
 
 void input() {
-    //scanf("%d %d", &N, &M);
-    cin >> N >> M;
-    for (int i = 0; i < N; i++) cin >> jews[i].first >> jews[i].second;
-    for (int i = 0; i < M; i++)cin >> bags[i];
+    scanf("%d %d", &N, &M);
+    for (int i = 0; i < N; i++)scanf("%d %d", &jews[i].first, &jews[i].second);
+    for (int i = 0; i < M; i++)scanf("%d", &bags[i]);
     sort(jews, jews + N);
-    sort(bags, bags + M);
+    sort(bags, bags+M);
 }
 
 int main() {
@@ -25,20 +23,18 @@ int main() {
     input();
 
     priority_queue<int> pq;
-
     for (int i = 0; i < M; i++) {
         for (int j = idx; j < N; j++) {
             if (jews[j].first > bags[i])break;
             idx++; pq.push(jews[j].second);
         }
-
         if (pq.size())
         {
             res += pq.top(); pq.pop();
         }
     }
 
-    cout << res;
+    printf("%lld", res);
     return 0;
 
 }
