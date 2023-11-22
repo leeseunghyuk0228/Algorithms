464e0bb3739b5038880e20cf9046770934323956
2seunghyuk <67091436+leeseunghyuk0228@users.noreply.github.com>
2023-11-22 17:48:03 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -2,16 +2,15 @@
 #include <algorithm>
 using namespace std;
 
-int cost[1000][3];
 int dp[1000][3];
 int MIN = 21e8;
 int N;
 
 int main() {
 	int t1,t2,t3;
+	int res = 21e8;
 	cin >> N;
 
-	// dp 초기값 채우고
 	cin >> dp[0][0] >> dp[0][1] >> dp[0][2];
 
 	for (int i = 1; i < N; i++) {
@@ -20,7 +19,7 @@ int main() {
 		dp[i][1] = t2 + min(dp[i - 1][0], dp[i - 1][2]);
 		dp[i][2] = t3 + min(dp[i - 1][0], dp[i - 1][1]);
 	}
-	int res = 21e8;
+	
 
 	for (int i = 0; i < 3; i++) {
 		res = min(res, dp[N - 1][i]);
