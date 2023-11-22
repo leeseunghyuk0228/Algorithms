f5cff71c58faec7f86f32658c809a77b1827ffbd
2seunghyuk <67091436+leeseunghyuk0228@users.noreply.github.com>
2023-11-22 15:29:43 +0900

    [Silver III] Title: 통계학, Time: 268 ms, Memory: 5140 KB -BaekjoonHub

---
+++ 백준/Silver/2108. 통계학/통계학.cc
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+
+int n, dat[8001];
+int mode_cnt;
+double SUM = 0;
+vector<int> v;
+
+void get_mean() {
+    int ans = round(SUM / n);
+    cout << ans <<'\n';
+}
+
+void get_median() {
+    cout << v[n / 2] - 4000<<'\n';
+}
+
+void get_mode() {
+    int flag = 0;
+    int ans=0;
+    for (int i = 0; i < 8001; i++) {
+        if (dat[i] == mode_cnt) {
+            ans = i;
+            flag++;
+        }
+        if (flag == 2)break;
+    }
+    cout << ans-4000 << '\n';
+}
+
+void get_minmax() {
+    
+    cout << v[n-1] - v[0] << '\n';
+}
+
+int main() {
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        double num;
+        cin >> num;
+        SUM += num;
+        int a = num+ 4000;
+        dat[a]++;
+
+        if (dat[a] > mode_cnt) mode_cnt = dat[a];
+        v.push_back(a);
+    }
+
+    sort(v.begin(), v.end());
+
+    get_mean();
+    get_median();
+    get_mode();
+    get_minmax();
+}
