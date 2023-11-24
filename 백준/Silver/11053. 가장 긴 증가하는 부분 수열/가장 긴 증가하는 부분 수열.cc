7805c1c8d55310ccd470666d084d82aa4640a4ce
2seunghyuk <67091436+leeseunghyuk0228@users.noreply.github.com>
2023-11-24 22:13:08 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -1,27 +1,29 @@
 #include <iostream>
-using namespace std;
-int N;
-int Arr[1010];
-int dp[1010];
-int MAX(int a, int b) { if (a > b)return a; return b; }
-void solution() {
-	int answer = 0;
-	for (int i = 1; i <= N; i++)
-	{
-		dp[i] = 1;
-		for (int j = i - 1; j >= 1; j--)
-		{
-			if (Arr[i] > Arr[j])
-				dp[i] = MAX(dp[i], dp[j] + 1);
-		}
-		answer = MAX(dp[i], answer);
-	}
-	cout << answer;
-}
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
 
-int main() {
+using namespace std;
+
+int N,arr[1001],dp[1001],ans=0;
+
+void input() {
 	cin >> N;
-	for (int i = 1; i <= N; i++)cin >> Arr[i];
-	solution();
+	for (int i = 1; i <= N; i++)
+		cin >> arr[i];
+}
+int main() {
+	input();
+	dp[1] = 1;
+
+	for (int i = 1; i <= N; i++) {
+		dp[i] = 1;
+		for (int j = i - 1; j >= 1; j--) 
+		{
+			if (arr[i] > arr[j]) 
+				dp[i] = MAX(dp[j] + 1, dp[i]);
+		}
+		ans = MAX(dp[i],ans);
+	}
+
+	cout << ans;
 	return 0;
 }
