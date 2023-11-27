6411e7efeaa2672bcb520166ff9f294eead91fe8
2seunghyuk <67091436+leeseunghyuk0228@users.noreply.github.com>
2023-11-27 15:38:14 +0900

    [Silver II] Title: 연속합, Time: 8 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#define MAX(a,b) ((a>b)?a:b)
+using namespace std;
+
+int ans,N,arr[100001],dp[100001];
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin >> N;
+
+	for (int i = 1; i <= N; i++) cin >> arr[i];
+
+	ans = dp[1] = arr[1];
+
+	for (int i = 2; i <= N; i++)
+	{
+		dp[i] = dp[i - 1] + arr[i];
+		dp[i] = MAX(dp[i], arr[i]);
+		ans = MAX(dp[i], ans);
+	}
+	cout << ans;
+
+}
