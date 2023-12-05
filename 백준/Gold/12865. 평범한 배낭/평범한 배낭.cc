c4602b83f95c88ed4ffd050a422f260c14de2ba7
2seunghyuk <67091436+leeseunghyuk0228@users.noreply.github.com>
2023-12-05 18:10:33 +0900

    [Gold V] Title: 평범한 배낭, Time: 36 ms, Memory: 41476 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -19,18 +19,12 @@ int main()
 	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 	input();
 
-	for (int w = 1; w <= max_weight; w++)
+	for (int i = 1; i <= N; i++)
 	{
-		for (int p = 1; p <= N; p++)
+		for (int j = 1; j <= max_weight; j++)
 		{
-			if (weights[p] > w)dp[p][w] = dp[p - 1][w];
-			else
-			{
-				int cmp1 = dp[p - 1][w - weights[p]] + values[p];
-				int cmp2 = dp[p - 1][w];
-				dp[p][w] = MAX(cmp1,cmp2);
-			}
-
+			if (weights[i] > j)dp[i][j] = dp[i - 1][j];
+			else dp[i][j] = MAX(dp[i - 1][j], dp[i - 1][j - weights[i]] + values[i]);
 		}
 	}
 
