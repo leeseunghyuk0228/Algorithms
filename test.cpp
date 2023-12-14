a853c8a405677e1fb4261b44b21b634e9e623e52
LEESEUNGHYUK <sh2463@gmail.com>
2023-12-14 13:54:38 +0900

    docs: 프로그래머스 코드 옮김

--- test.cpp
+++ test.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 using namespace std;
-
+int N,dp[101],value[101];
 int main(){
-    int n=1;
-    while(n<=10)
+    cin>>N;
+    for(int i=1;i<=N;i++) cin>>value[i];
+    dp[1]=1; // 1 (1)
+    dp[2]=2;// 11 2(2)
+    for(int i=3;i<=N;i++)
     {
-        n++;
+        if(i%2==0)dp[i]= dp[i/2]*2+1;
+        else dp[i] = dp[i-1]+1;
     }
+    cout<<dp[N];
     return 0;
 }
