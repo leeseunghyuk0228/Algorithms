f6578fbf46bf940d11e5988bde62e2e3dfa07c62
2seunghyuk <67091436+leeseunghyuk0228@users.noreply.github.com>
2023-12-15 14:39:40 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 128 ms, Memory: 10228 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -5,7 +5,7 @@ using namespace std;
 int n,m,num[1025][1025],dp[1025][1025];
 
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);cout.tie(0);
     cin >> n>>m;
 
     for (int i = 1; i <= n; i++)
@@ -14,15 +14,14 @@ int main() {
 
     dp[1][1] = num[1][1];
 
-    for (int i = 2; i <= n; i++)
-    {
-        dp[i][1] = dp[i - 1][1] + num[i][1];
-        dp[1][i] = dp[1][i - 1] + num[1][i];
-    }
-
-    for (int i = 2; i <= n; i++)
-        for (int j = 2; j <= n; j++)
-            dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i-1][j-1]+num[i][j];
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++)
+        {
+            if (i == 1 && j == 1)continue;
+            if (i == 1) dp[i][j] = dp[i][j-1] + num[i][j];
+            else if (j == 1)dp[i][j] = dp[i-1][j] + num[i][j];
+            else dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + num[i][j];
+        }
 
     for (int i = 0; i < m; i++)
     {
