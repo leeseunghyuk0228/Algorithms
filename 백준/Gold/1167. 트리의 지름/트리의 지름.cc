b27a90dd414661e5e6e556d43b124c5c35c0b1a6
2seunghyuk <67091436+leeseunghyuk0228@users.noreply.github.com>
2024-01-22 18:08:55 +0900

    [Gold II] Title: 트리의 지름, Time: 100 ms, Memory: 11004 KB -BaekjoonHub

---
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N,res;
+vector<pair<int,int>> v[100001];
+int visited[100001];
+int next_start;
+
+void input()
+{
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		int from, to, weight;
+		cin >> from;
+		while (1)
+		{
+			cin >> to;
+			if (to == -1)break;
+			cin >> weight;
+			v[from].push_back({ to,weight });
+			v[to].push_back({ from,weight });
+		}
+	}
+}
+
+void dfs(int start,int now_weight)
+{
+	visited[start] = 1;
+
+	if (now_weight > res)
+	{
+		res = now_weight;
+		next_start = start;
+	}
+	for (auto n : v[start])
+	{
+		if (visited[n.first])continue;
+		dfs(n.first,now_weight + n.second);
+	}
+
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+	input();
+	dfs(1,0);
+	for (int i = 1; i <= N; i++)visited[i] = 0;
+	dfs(next_start, 0);
+
+	cout << res;
+}
